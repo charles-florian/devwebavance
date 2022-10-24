a210543e3ee93e56cb81f383ec470b02511f25e3
chanok001 <charlyvanacker@gmail.com>
2022-10-24 14:13:47 +0200

    TestAffichage

---
+++ src/main/java/be/heh/projet_dev_web/adaptater/in/TournamentControler.java
@@ -0,0 +1,4 @@
+package be.heh.projet_dev_web.adaptater.in;
+
+public class TournamentControler {
+}
---
+++ src/test/java/be/heh/projet_dev_web/TestAffiche.java
@@ -0,0 +1,54 @@
+package be.heh.projet_dev_web;
+
+import be.heh.testarchitecturehexa.adaptater.in.StudentController;
+import be.heh.testarchitecturehexa.model.Student;
+import be.heh.testarchitecturehexa.port.in.StudentListUseCase;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasItemInArray;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+//@SpringBootTest
+//@RunWith(SpringRunner.class)
+@WebMvcTest(TournamentController.class)
+
+public class TestAffiche {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private StudentListUseCase studentListUseCase;
+
+    private List<Student> students = new ArrayList<>();
+
+    @Test
+    public void testStudentController() throws Exception {
+
+        students.add(new Student("tata","titi", LocalDate.of(2010,03,24)));
+        students.add(new Student("tata1","titi1", LocalDate.of(2011,04,25)));
+        students.add(new Student("tata2","titi2", LocalDate.of(2012,05,26)));
+
+        //Stub
+        Mockito.when(studentListUseCase.getStudentList()).thenReturn(students);
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("studentList"))
+                .andExpect(model().attributeExists("students"))
+                .andExpect(model().attribute("students", Matchers.hasSize(3)));
+    }
+}
