d080301cb45af596e71485195c169b5e14413a7f
MERFlorian <florian.mercier@std.heh.be>
2022-11-30 14:52:44 +0100

    test updatePersistence

--- src/test/java/be/heh/projet_dev_web/UpdatePersistenceTest.java
+++ src/test/java/be/heh/projet_dev_web/UpdatePersistenceTest.java
@@ -1,5 +1,6 @@
 package be.heh.projet_dev_web;
 
+
 import be.heh.projet_dev_web.adaptater.out.*;
 import be.heh.projet_dev_web.model.Tournament;
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -12,7 +13,6 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -22,31 +22,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-
-public class AddPersistenceTests extends AbstractIntegrationTest {
-
-    private Tournament tournament=new Tournament("VALORANT","LocalDate.now()",15.5,10);
-
+public class UpdatePersistenceTest {
     @Autowired
     private TournamentRepository tournamentRepository;
-    private AddPersistenceAdaptater addPersistenceAdaptater;
     private TournamentMapper tournamentMapper;
+
+
+    private UpdatePersistenceAdaptater updatePersistenceAdaptater;
     private TournamentPersistenceAdapter tournamentPersistenceAdapter;
 
-
-
     @Test
-    @Sql("createTournamentTable.sql")
-    void addTournament(){
+    @Sql({"createTournamentTable.sql"})
+    void getTournamentList(){
 
-
-
+        Tournament tournament=new Tournament("VALORANT","747",21.2,1);
+        AddPersistenceAdaptater addPersistenceAdaptater = new AddPersistenceAdaptater(tournamentRepository);
+        addPersistenceAdaptater.addTournament(tournament);
 
         tournamentMapper=new TournamentMapper();
         tournamentPersistenceAdapter=new TournamentPersistenceAdapter(tournamentRepository,tournamentMapper);
         List<Tournament> tournaments;
         tournaments=tournamentPersistenceAdapter.getTournamentList();
-        assertEquals("VALORANT",tournaments.get(tournaments.size()-1).getNom());
+        updatePersistenceAdaptater=new UpdatePersistenceAdaptater(tournamentRepository,tournamentMapper);
+        Tournament t=new Tournament("CSGO","747",21.2,1);
+        updatePersistenceAdaptater.updateTournament(t);
+        tournaments=tournamentPersistenceAdapter.getTournamentList();
+
+        assertEquals("CSGO",tournaments.get(tournaments.size()-1).getNom());
     }
+
 
 }
